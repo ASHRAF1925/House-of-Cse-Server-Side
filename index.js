955715a61fa1e4e11da81e157dbe99459cab0cef
ASHRAF1925 <1925ashraf@gmail.com>
2022-10-25 22:45:38 +0600

    courses added

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const { request } = require('express');
 const express=require('express')
 const app=express()
 const port=process.env.PORT || 5000;
+
+const courses=require('./data/Courses.json');
+const instructors=require('./data/Instructors.json')
+
+
 app.get('/',(req,res)=>{
     res.send('API RUNNING')
 });
+app.get('/courses',(req,res)=>{
+    res.send(courses)
+});
+
+
+
 app.listen(port,()=>{
     console.log('Server is running on port',port);
 })
