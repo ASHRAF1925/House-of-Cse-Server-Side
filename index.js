5efd383db5384406ecb7910cef8f86eaf22e3785
ASHRAF1925 <1925ashraf@gmail.com>
2022-10-25 16:39:57 +0600

    exprees is done

---
+++ index.js
@@ -0,0 +1,10 @@
+const { request } = require('express');
+const express=require('express')
+const app=express()
+const port=process.env.PORT || 5000;
+app.get('/',(req,res)=>{
+    res.send('API RUNNING')
+});
+app.listen(port,()=>{
+    console.log('Server is running on port',port);
+})
